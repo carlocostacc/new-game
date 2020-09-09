43c27a3698b96610cbdc19ee0866a24c5b612459
carlocostacc <49722518+carlocostacc@users.noreply.github.com>
2020-09-09 17:07:35 -0400

    Add files via upload

---
+++ scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float move_speed;
+    private Vector2 move_input;
+    public Rigidbody2D theRB;
+    public Transform stafarm;
+    private Camera theCam;
+    public Animator anim;
+    public GameObject bulletTofire;
+    public Transform firepoint;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //getaxis does acceleration
+        move_input.x = Input.GetAxisRaw("Horizontal");
+        move_input.y = Input.GetAxisRaw("Vertical");
+
+
+        move_input.Normalize();
+        //transform.position += new Vector3(move_input.x * Time.deltaTime * move_speed, move_input.y * Time.deltaTime * move_speed, 0f);
+        theRB.velocity = move_input * move_speed;
+
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+        if (mousePos.x < screenPoint.x){
+            transform.localScale = new Vector3(-1f, 1f,1f);
+            stafarm.localScale = new Vector3(-1F,1F,1F);
+        }
+        else {
+            transform.localScale = Vector3.one;
+            stafarm.localScale = Vector3.one;
+        }
+
+        // fix rotation of weapon to follow mouse position 
+        // rotation gun arm
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y ,offset.x) * Mathf.Rad2Deg;
+        stafarm.rotation = Quaternion.Euler(0, 0, angle - 18);
+
+        if (Input.GetMouseButtonDown(0)){
+            Instantiate(bulletTofire, firepoint.position, firepoint.rotation);
+        }
+
+
+        // print finction to console **Debug.Log("Text: " + myText.text); **
+        Debug.Log("angle: " + -1*angle);
+
+        if (move_input != Vector2.zero){
+            anim.SetBool("isMoving",true);
+        }
+        else{
+            anim.SetBool("isMoving", false);
+        }
+    }
+}
---
+++ scripts/playerbullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerbullet : MonoBehaviour
+{
+
+    public float speed = 7.5f;
+    public Rigidbody2D theRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRb.velocity = transform.right * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D other){
+        Destroy(gameObject);
+    }
+}
